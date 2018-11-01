774d0f13f489fca9209683debd10bcdc875f0db5
Qiang <qning2@illinois.edu>
2018-11-01 14:25:18 -0500

    add flag to select curator or pipeline in myTextPreprocessor

--- src/main/java/edu/illinois/cs/cogcomp/temporal/TemporalRelationExtractor/myTextPreprocessor.java
+++ src/main/java/edu/illinois/cs/cogcomp/temporal/TemporalRelationExtractor/myTextPreprocessor.java
@@ -3,6 +3,7 @@ package edu.illinois.cs.cogcomp.temporal.TemporalRelationExtractor;
 import edu.illinois.cs.cogcomp.annotation.AnnotatorService;
 import edu.illinois.cs.cogcomp.core.datastructures.ViewNames;
 import edu.illinois.cs.cogcomp.core.datastructures.textannotation.*;
+import edu.illinois.cs.cogcomp.curator.CuratorFactory;
 import edu.illinois.cs.cogcomp.nlp.tokenizer.StatefulTokenizer;
 import edu.illinois.cs.cogcomp.nlp.utility.TokenizerTextAnnotationBuilder;
 import edu.illinois.cs.cogcomp.pipeline.main.PipelineFactory;
@@ -12,34 +13,70 @@ import java.util.List;
 public class myTextPreprocessor {
     private AnnotatorService annotator;
     private TokenizerTextAnnotationBuilder tab;
+    private boolean useCuratorOrPipeline = true; // true->curator. false->pipeline
 
     public myTextPreprocessor() throws Exception{
-        annotator = PipelineFactory.buildPipeline(
-                ViewNames.POS,
-                ViewNames.SHALLOW_PARSE,
-                ViewNames.PARSE_STANFORD,
-                ViewNames.NER_CONLL,
-                ViewNames.LEMMA,
-                ViewNames.SRL_VERB
-        );
+        if(useCuratorOrPipeline)
+            annotator = CuratorFactory.buildCuratorClient();
+        else
+            annotator = PipelineFactory.buildPipeline(
+                    ViewNames.POS,
+                    ViewNames.SHALLOW_PARSE,
+                    ViewNames.PARSE_STANFORD,
+                    ViewNames.NER_CONLL,
+                    ViewNames.LEMMA,
+                    ViewNames.SRL_VERB
+            );
         tab = new TokenizerTextAnnotationBuilder(new StatefulTokenizer());
     }
 
     public TextAnnotation extractTextAnnotation(String text)  throws Exception{
-        TextAnnotation ta = tab.createTextAnnotation(text);
-        TextAnnotation[] sentTa = new TextAnnotation[ta.sentences().size()];
-        for(int sentenceId=0;sentenceId<ta.sentences().size();sentenceId++){// without this, empty views of ta will be added
-            sentTa[sentenceId]= TextAnnotationUtilities.getSubTextAnnotation(ta, sentenceId);
-            annotator.annotateTextAnnotation(sentTa[sentenceId], true);
+        TextAnnotation ta;
+        if(useCuratorOrPipeline){
+            ta = annotator.createBasicTextAnnotation("","", text);
+            try {
+                annotator.addView(ta, ViewNames.SENTENCE);
+            }
+            catch (Exception e){
+                System.out.printf("Adding SENTENCE view failed for text %s\n",text);
+                e.printStackTrace();
+            }
+            TextAnnotation[] sentTa = new TextAnnotation[ta.sentences().size()];
+            for(int sentenceId=0;sentenceId<ta.sentences().size();sentenceId++){// without this, empty views of ta will be added
+                sentTa[sentenceId]= TextAnnotationUtilities.getSubTextAnnotation(ta, sentenceId);
+            }
+            for (int sentenceId = 0; sentenceId < ta.sentences().size(); ++sentenceId) {
+                try {
+                    annotator.addView(sentTa[sentenceId], ViewNames.LEMMA);
+                    annotator.addView(sentTa[sentenceId], ViewNames.POS);
+                    annotator.addView(sentTa[sentenceId], ViewNames.SRL_VERB);
+                    annotator.addView(sentTa[sentenceId], ViewNames.DEPENDENCY);
+                /*annotator.addView(sentTa[sentenceId], ViewNames.SRL_NOM);
+                annotator.addView(sentTa[sentenceId], ViewNames.SRL_PREP);*/
+                } catch (Exception e) {
+                    System.out.printf("Adding SRL view failed for doc %s sentId=%d\n","",sentenceId);
+                    e.printStackTrace();
+                }
+                int start = ta.getSentence(sentenceId).getStartSpan();
+                int end = ta.getSentence(sentenceId).getEndSpan();
+                myTextAnnotationUtilities.copyViewsFromTo(sentTa[sentenceId],ta, start, end, start);
+            }
         }
-        for (int sentenceId = 0; sentenceId < ta.sentences().size(); ++sentenceId) {
-            int start = ta.getSentence(sentenceId).getStartSpan();
-            int end = ta.getSentence(sentenceId).getEndSpan();
-            myTextAnnotationUtilities.copyViewsFromTo(sentTa[sentenceId],ta, start, end, start);
+        else {
+            ta = tab.createTextAnnotation(text);
+            TextAnnotation[] sentTa = new TextAnnotation[ta.sentences().size()];
+            for (int sentenceId = 0; sentenceId < ta.sentences().size(); sentenceId++) {// without this, empty views of ta will be added
+                sentTa[sentenceId] = TextAnnotationUtilities.getSubTextAnnotation(ta, sentenceId);
+                annotator.annotateTextAnnotation(sentTa[sentenceId], true);
+            }
+            for (int sentenceId = 0; sentenceId < ta.sentences().size(); ++sentenceId) {
+                int start = ta.getSentence(sentenceId).getStartSpan();
+                int end = ta.getSentence(sentenceId).getEndSpan();
+                myTextAnnotationUtilities.copyViewsFromTo(sentTa[sentenceId], ta, start, end, start);
+            }
         }
         return ta;
     }
-
     public static void main(String[] args) throws Exception{
         myTextPreprocessor exc = new myTextPreprocessor();
         String text = "I failed to do it.";
